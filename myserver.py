730538614fa6f5bf5ef5aef5319ce0f05845ba63
Nellxz <betraylike02@gmail.com>
2024-10-05 20:30:58 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -8,8 +8,8 @@ def home():
     return "Server is running!"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=8080)
 
 def server_on():
     t = Thread(target=run)
-    t.start
+    t.start()
